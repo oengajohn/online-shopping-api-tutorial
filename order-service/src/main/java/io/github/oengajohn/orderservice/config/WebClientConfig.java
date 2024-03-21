0fc10fbb15929884d3040a7f8ffe9855b69a05bb
John Oenga Ongwae <johnongwae4@gmail.com>
2024-03-21 08:22:28 +0300

    Add load balanced configuration to the service for webclient builder

--- order-service/src/main/java/io/github/oengajohn/orderservice/config/WebClientConfig.java
+++ order-service/src/main/java/io/github/oengajohn/orderservice/config/WebClientConfig.java
@@ -1,5 +1,6 @@
 package io.github.oengajohn.orderservice.config;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -9,6 +10,7 @@ public class WebClientConfig {
 
 
     @Bean
+    @LoadBalanced
     WebClient.Builder webClientBuilder(){
         return WebClient.builder();
     }
