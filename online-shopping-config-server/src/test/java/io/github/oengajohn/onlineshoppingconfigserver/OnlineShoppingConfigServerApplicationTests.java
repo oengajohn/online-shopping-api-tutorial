1211aed5433af52748c36c3a653b601e32c6baff
John Oenga Ongwae <johnongwae4@gmail.com>
2024-03-22 08:07:32 +0300

    Add config server for centralized configuration management

---
+++ online-shopping-config-server/src/test/java/io/github/oengajohn/onlineshoppingconfigserver/OnlineShoppingConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.oengajohn.onlineshoppingconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineShoppingConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
